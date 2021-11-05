6337cd21b916ec46fe9f0f3c3d24e46d145bf07a
depotte6 <depotte6@gmail.com>
2021-11-05 18:55:32 -0400

    route work

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
+const seqquelize = require('../config/connection');
 
 router.get('/', withAuth, async (req, res) => {
   try {
@@ -31,11 +32,119 @@ router.get('/signup', (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/dashboard');
     return;
   }
 
   res.render('login');
+});
+
+router.get('/dashboard', withAuth, async(req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'body',
+      'timestamp'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['body', 'time_stamp'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }
+      ]
+    })
+    .then(dbPostData => {
+      const posts = dbPostData.map(post -> post.get({ plain: true }));
+      res.render('dashboard', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+
+});
+
+router.get('/edit/:id', withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'title',
+      'timestamp',
+      'body'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['body', 'timestamp'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }
+    ]
+  })
+    .then(dbPostData => {
+      if (!dbPostData) {
+        res.status(404).json({ message: 'No post found with this id' });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      res.render('edit-post', {
+          post,
+          loggedIn: true
+          });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/create/', withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      // use the ID from the session
+      user_id: req.session.user_id
+    },
+    attributes: [
+      'id',
+      'title',
+      'timestamp',
+      'body'
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['body', 'timestamp'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      }
+    ]
+  })
+    .then(dbPostData => {
+      // serialize data before passing to template
+      const posts = dbPostData.map(post => post.get({ plain: true }));
+      res.render('create-post', { posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
---
+++ models/Comment.js
@@ -0,0 +1,32 @@
+const{ Model, DataTypes } = require('sequelize')
+const sequelize = require('../config/connection')
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    title: {
+      type: DataTypes.STRING
+    },
+    body: {
+      type: DataTypes.STRING,
+    },
+  },
+    {
+    
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+        
+
+  }
+);
+
+module.exports= Comment
