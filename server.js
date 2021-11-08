3f257fd340a06c370ac217cca54cd6e4deb549ea
depotte6 <depotte6@gmail.com>
2021-11-07 22:31:13 -0500

    h

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers });
+
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
