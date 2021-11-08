2181660ca2e1244405b7efef9edace4c1adbe7a2
depotte6 <depotte6@gmail.com>
2021-11-07 22:29:07 -0500

    h

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 const routes = require('./controllers');
 
 const sequelize = require('./config/connection');
@@ -14,7 +15,7 @@ const hbs = exphbs.create();
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: { maxAge: 36000 },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
