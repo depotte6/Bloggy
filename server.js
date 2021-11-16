bfed379d3c0b1876b78f2d702d211cce05ad2e1c
depotte6 <depotte6@gmail.com>
2021-11-16 18:35:33 -0500

    h

--- server.js
+++ server.js
@@ -1,39 +1,35 @@
 const express = require('express');
 const path = require('path');
-//paths
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-//Session
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const helpers = require('./utils/helper');
+
+const app = express();
+const PORT = process.env.PORT || 4001;
 const hbs = exphbs.create({ helpers });
-//set up the actual session
+
 const sess = {
   secret: 'super secret secret',
-  cookie: {
-  },
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
     db: sequelize,
   }),
 };
-//initialize the server
-const app = express();
-const PORT = process.env.PORT || 4001;
 
-//middlewear
+
 app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-//set handlebars as render engine
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
