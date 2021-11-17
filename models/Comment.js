70ef0bba6f339abbc16554e8fe0303370d974374
depotte6 <depotte6@gmail.com>
2021-11-16 19:31:12 -0500

    h

--- models/Comment.js
+++ models/Comment.js
@@ -1,4 +1,4 @@
-const{  Model, DataTypes } = require('sequelize')
+const { Model, DataTypes } = require('sequelize')
 const sequelize = require('../config/connection')
 
 class Comment extends Model {}
@@ -18,7 +18,7 @@ Comment.init(
     },
   },
     {
-         sequelize,
+        sequelize,
         timestamps: false,
         freezeTableName: true,
         underscored: true,
